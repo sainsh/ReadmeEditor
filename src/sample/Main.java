9c26ffaebaaa96afad18a13f04230026ec3c2fac
sainsh <kfsainsh@gmail.com>
2018-11-22 11:26:47 +0100

    cosmetic changes + added check if readme.md exists

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,14 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        File readMe = new File("README.md");
+        if(readMe.exists()) {
+
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 500, 400));
+            primaryStage.show();
+        }
+        else{
+            PrintWriter writer = new PrintWriter(readMe);
+            writer.close();
+            System.exit(0);
+        }
     }
 
 
