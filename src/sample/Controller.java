d1b230bbf1dab12377cf1627fe9c12593cbdc0ee
sainsh <kfsainsh@gmail.com>
2018-11-22 10:38:47 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+
+    public void onLoadBTNClick(ActionEvent actionEvent) {
+    }
+
+    public void onSaveBTNClick(ActionEvent actionEvent) {
+    }
+}
